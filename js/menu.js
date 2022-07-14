9e6db81b5cf34181ea659a1ab2ada572d5b8b470
freireluan <94190436+freireluan@users.noreply.github.com>
2022-07-14 13:16:32 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,10 @@
 var botaoMenu = document.querySelector("#checkbox-menu");
 var menu = document.querySelector(".form")
-
+var altura = window. screen. height;
+var largura = window. screen. width;
+console.log(largura, altura);
 
 botaoMenu.addEventListener("click", ()=>{
-    setTimeout(() => {
-        if (botaoMenu.checked){
-            menu.classList.remove("menuDesativado");
-            menu.classList.add("menuAtivo");
-            menu.classList.add("form");
-            
-        }else{
-            menu.classList.remove("form");
-            menu.classList.remove("menuAtivo")
-            menu.classList.add("menuDesativado");
-          
-        }
-        
-    }, 1);
-    
     if (botaoMenu.checked){
         menu.classList.remove("menuDesativado");
         menu.classList.add("menuAtivo");
@@ -32,6 +19,34 @@ botaoMenu.addEventListener("click", ()=>{
     
     
 })
+
+
+setTimeout(() => {
+    if (botaoMenu.checked){
+        menu.classList.remove("menuDesativado");
+        menu.classList.add("menuAtivo");
+        menu.classList.add("form");
+        
+    }else{
+        menu.classList.remove("form");
+        menu.classList.remove("menuAtivo")
+        menu.classList.add("menuDesativado");
+      
+    }
+    
+}, 1);
+setInterval(() => {
+    largura = window. screen. width;
+   if(largura > 800) {
+    menu.classList.add("form");
+    menu.classList.remove("menuDesativado");
+   }else if(largura <=  800){
+    menu.classList.remove("form");
+    menu.classList.add("menuDesativado")
+   }
+},3);
+
+
 
 
   
