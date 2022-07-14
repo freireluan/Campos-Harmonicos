3dfc3cf1a2238ec36f326936f57f5d56823fe27d
freireluan <94190436+freireluan@users.noreply.github.com>
2022-07-14 13:09:42 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,19 +1,8 @@
 var botaoMenu = document.querySelector("#checkbox-menu");
 var menu = document.querySelector(".form")
-setTimeout(() => {
-    if (botaoMenu.checked){
-        menu.classList.remove("menuDesativado");
-        menu.classList.add("menuAtivo");
-        menu.classList.add("form");
-        
-    }else{
-        menu.classList.remove("form");
-        menu.classList.remove("menuAtivo")
-        menu.classList.add("menuDesativado");
-      
-    }
-    
-}, 1);
+var altura = window. screen. height;
+var largura = window. screen. width;
+
 
 botaoMenu.addEventListener("click", ()=>{
     if (botaoMenu.checked){
@@ -30,6 +19,33 @@ botaoMenu.addEventListener("click", ()=>{
     
     
 })
+
+
+setTimeout(() => {
+    if (botaoMenu.checked){
+        menu.classList.remove("menuDesativado");
+        menu.classList.add("menuAtivo");
+        menu.classList.add("form");
+        
+    }else{
+        menu.classList.remove("form");
+        menu.classList.remove("menuAtivo")
+        menu.classList.add("menuDesativado");
+      
+    }
+    
+}, 1);
+// setTimeout(() => {
+//    if(largura > 800) {
+//     menu.classList.add("form");
+//     menu.classList.remove("menuDesativado");
+//    }else{
+//     menu.classList.remove("form");
+//     menu.classList.add("menuDesativado")
+//    }
+// },3);
+
+
 
 
   
